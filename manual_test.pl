05adda7ec604390d6fa65b7d7fe84ffee835b060
Michael Hendricks <michael@ndrix.org>
2013-01-25 14:46:56 -0700

    Support arithmetic functions
    Arithmetic expressions of a single variable can be used (nearly)
    anywhere that functions are expected.

--- manual_test.pl
+++ manual_test.pl
@@ -2,7 +2,12 @@
 :- use_module(func, [op(675,xfy,$),op(650,xfy,of)]).
 
 main :-
-    Plus3 = succ of succ of plus(1),
+    format('~s world~n', [atom_codes $ hello]),
+    Plus3 = succ of _+1 of plus(1),
     call(Plus3, 1, 4),
-
     format('The answer: ~p~n', [succ of plus(40) $ 1]).
+
+fact(0,1) :- !.
+fact(N,F) :-
+    N > 0,
+    F is N * (fact of _-1 $ N).
--- prolog/func.pl
+++ prolog/func.pl
@@ -3,14 +3,18 @@
                 ]).
 :- use_module(library(list_util), [xfy_list/3]).
 :- use_module(library(function_expansion)).
+:- use_module(library(arithmetic)).
 
 
 % for the cross-referencer. removed during macro expansion
 :- meta_predicate $(2,+).
 $(_,_).
 
-% Y is the result of applying function F to argument X
-user:function_expansion($(F,X), Y, func:Apply) :-  % basic functions
+user:function_expansion($(F,X), Y, Y is F) :-  % single var arithmetic functions
+    arithmetic:evaluable(F),
+    !,
+    term_variables(F, [X]).
+user:function_expansion($(F,X), Y, func:Apply) :-  % composed functions
     func:function_composition_term(F),
     function_expansion(F, func:Functor, true),
     Apply =.. [Functor,X,Y].
@@ -25,6 +29,24 @@ user:function_expansion($(F,X), Y, Apply) :-  % basic functions
 :- meta_predicate of(2,2).
 of(_,_).
 
+% Converts an evaluable arithmetic expression of one variable
+% into a predicate of two variables.  For all else, F=P.
+% Identical expressions are compiled only once.
+to_predicate(F,P) :-
+    arithmetic:evaluable(F),
+    !,
+    term_variables(F, [X]),
+    variant_sha1(F, Hash),  % term_hash/2 supports only ground terms
+    format(atom(P), 'arithmetic_function_~p', [Hash]),
+    (   current_predicate(P/2)
+    ->  true  % predicate already exists
+    ;   Head =.. [P,X,Y],
+        assert((Head :- Y is F)),
+        compile_predicates([P/2])
+    ).
+to_predicate(F,F).
+
+
 % True if the argument is a function composition term
 function_composition_term(of(_,_)).
 
@@ -32,7 +54,8 @@ function_composition_term(of(_,_)).
 functions_to_compose(Term, Funcs) :-
     functor(Term, Op, 2),
     Op = 'of',
-    xfy_list(Op, Term, Funcs).
+    xfy_list(Op, Term, Raw),
+    maplist(to_predicate, Raw, Funcs).
 
 user:function_expansion(Term, func:Functor, true) :-
     functions_to_compose(Term, Funcs),
@@ -54,6 +77,9 @@ test(multiple) :-
     16 = X.
 test(arithmetic) :-
     8 =:= 2*(succ $ 3).
+test(evaluable_functions) :-
+    X = _+1 $ 0,
+    X =:= 1.
 :- end_tests(apply).
 
 :- begin_tests(compose).
@@ -69,6 +95,9 @@ test(idempotent) :-
     G = F,
     call(F, 1, X),
     call(G, 1, X).
+test(evaluable_functions) :-
+    Plus3 = succ of _+1 of plus(1),
+    call(Plus3, 0, 3).
 :- end_tests(compose).
 
 :- begin_tests(compose_apply).
