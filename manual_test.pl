97b1bf95ce855cc63ad6a9aafc6196d014804c04
Michael Hendricks <michael@ndrix.org>
2013-02-16 16:31:52 -0700

    Flesh out documentation
    The previous documentation was sparse relying mostly on examples.
    This documentation explains nearly everything the library can do, why
    and how to extend it.

--- manual_test.pl
+++ manual_test.pl
@@ -1,11 +1,18 @@
 #!/usr/bin/env swipl -q -t main -s
 :- use_module(func).
 
+:- multifile func:compile_function/4.
+func:compile_function(List, Index, Elem, nth0(Index, List, Elem)) :-
+    is_list(List).
+
 main :-
     format('~s world~n', [atom_codes $ hello]),
     Plus3 = succ of _+1 of plus(1),
     call(Plus3, 1, 4),
-    format('The answer: ~p~n', [succ of plus(40) $ 1]).
+    format('The answer: ~p~n', [succ of plus(40) $ 1]),
+    N = 2,
+    format('Brought to you by the number ~w~n', [zero,one,two,three] $ N).
+
 
 fact(0,1) :- !.
 fact(N,F) :-
