9c6defb8192bf9abaa5f58c4ae14d2e21db3b63c
Michael Hendricks <michael@ndrix.org>
2013-06-07 12:45:28 -0600

    Rewrite manual_test using TAP
    By having the test generate TAP output, we can piggyback on all the
    excellent tools available for running and analyzing TAP tests.
    Eventually, all the TAP-specific code should be factored out to its
    own pack so that I can easily reuse it across projects.

--- manual_test.pl
+++ manual_test.pl
@@ -1,18 +1,54 @@
 #!/usr/bin/env swipl -q -t main -s
 :- use_module(func).
+:- nb_setval(tap_test_count, 0).
 
 :- multifile func:compile_function/4.
 func:compile_function(List, Index, Elem, nth0(Index, List, Elem)) :-
     is_list(List).
 
 main :-
-    format('~s world~n', [atom_codes $ hello]),
-    Plus3 = succ of _+1 of plus(1),
-    call(Plus3, 1, 4),
-    format('The answer: ~p~n', [succ of plus(40) $ 1]),
-    N = 2,
-    format('Brought to you by the number ~w~n', [zero,one,two,three] $ N).
+    writeln('TAP version 13'),
+    writeln('1..5'),
+    ok(hello_world),
+    ok(plus3),
+    ok(compose_apply),
+    ok(indexing),
+    ok(factorial).
 
+ok(Test) :-
+    ( call(Test) ->
+        test_result(ok, Test)
+    ; % otherwise ->
+        test_result('not ok', Test)
+    ).
+
+test_result(Status, Test) :-
+    nb_getval(tap_test_count, N0),
+    succ(N0, N),
+    format('~w ~w - ~w~n', [Status, N, Test]),
+    nb_setval(tap_test_count, N).
+
+hello_world :-
+    X = atom_codes $ hello,
+    X = "hello".
+
+plus3 :-
+    Plus3 = succ of _+1 of plus(1),
+    call(Plus3, 1, X),
+    X = 4.
+
+compose_apply :-
+    X = succ of plus(40) $ 1,
+    X =:= 42.
+
+indexing :-
+    N = 2,
+    Atom = [zero,one,two,three] $ N,
+    Atom = two.
+
+factorial :-
+    fact(7,Factorial),
+    Factorial =:= 5040.
 
 fact(0,1) :- !.
 fact(N,F) :-
